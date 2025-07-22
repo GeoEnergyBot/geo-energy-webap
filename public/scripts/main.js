6b8718a158720619ac7127635c34cc43c14ef2f9
GeoEnergyBot <deum.kz@gmail.com>
2025-07-22 22:37:02 +0500

    11
    1

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -77,7 +77,7 @@ async function collectEnergy(point, playerLat, playerLng, marker) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJ...твой_anon_key...'
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0a3pzcmxpY2ZodWZkbmVnd2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0NzA3NjAsImV4cCI6MjA2ODA0Njc2MH0.eI0eF_imdgGWPLiUULTprh52Jo9P69WGpe3RbCg3Afo'
     },
     body: JSON.stringify({
       action: "collect",
