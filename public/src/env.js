ec624af576aa375b4c236c60b8c64864d11e94d2
GeoEnergyBot <deum.kz@gmail.com>
2025-08-22 13:59:17 +0500

    Update env.js

--- public/src/env.js
+++ public/src/env.js
@@ -1,6 +1,6 @@
 
 export const SUPABASE_URL = 'https://ptkzsrlicfhufdnegwjl.supabase.co';
-export const SUPABASE_ANON = '';
+export const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0a3pzcmxpY2ZodWZkbmVnd2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0NzA3NjAsImV4cCI6MjA2ODA0Njc2MH0.eI0eF_imdgGWPLiUULTprh52Jo9P69WGpe3RbCg3Afo';
 export const FUNCTIONS_ENDPOINT = 'https://ptkzsrlicfhufdnegwjl.functions.supabase.co/generate-points'; // optional; if blank, будет использоваться локальная генерация точек
 
 export const GHOST_ICON_BASES = ['ghost_icons', 'assets/ghosts'];
