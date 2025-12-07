96eb61719410804f3808fb88777bf3d672040b0b
GeoEnergyBot <deum.kz@gmail.com>
2025-12-07 12:36:03 +0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -29,14 +29,14 @@ quests.init();
     try{ anti.updatePosition(lat, lng, Date.now()); }catch(e){}
     if (!map){
       const base = buildBaseLayers();
-      map = L.map('map', { center:[lat,lng], zoom:16, layers:[base.cartoDark] });
-      L.control.layers({ 'Carto Dark': base.cartoDark, 'OSM': base.osm, 'ESRI Спутник': base.esriSat }, null, { position:'topright', collapsed:true }).addTo(map);
+      map = L.map('map', { center:[lat,lng], zoom:16, layers:[base.spiritStyle] });
+      L.control.layers({ 'Мир духов': base.spiritStyle, 'Классическая карта': base.osm }, null, { position:'topright', collapsed:true }).addTo(map);
       playerMarker = L.marker([lat,lng], { icon: ghostIcon }).addTo(map).bindPopup('Вы здесь');
       lastTileId = getTileId(lat,lng);
       store.init(map, playerMarker);
       hotzones.init(map);
       const p = await loadOrCreatePlayer(user);
-      await updatePlayerHeader({ username: user.first_name||user.username||'Игрок', level: p.level, energy: p.energy, energy_max: p.energy_max });
+      await updatePlayerHeader({ username: user.first_name||user.username, level: p.level, energy: p.energy, energy_max: p.energy_max });
       await loadEnergyPoints(map, playerMarker, user);
     } else {
       playerMarker.setLatLng([lat,lng]);
